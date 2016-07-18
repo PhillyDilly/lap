89bc771993454dafbfe27682551e7c5be1155591
Philly Dilly <tonemontone@gmail.com>
2016-07-18 14:07:12 -0400

    update

--- filedemo/manifests/init.pp
+++ filedemo/manifests/init.pp
@@ -1,5 +1,28 @@
 class filedemo {
+	
+	File { 			# Default attributes
+		owner => 'root',
+		group => 'finance',
+		mode  => '0660',
+	}
 
+	$homedir = "/root"
+	$content = "my files content"
+
+	file { "${homedir}/myfile.txt":
+		content => $content,
+	}
+
+	file { "${homedir}/myfile2.txt":
+		content => "myfile2 content",
+	}
+
+	file { "${homedir}/myfile3.txt":
+		content => "myfile3 content",
+		owner   => admin,
+		group   => root,
+	}
+	
 	file { '/root/motd':
 		ensure  => present,
 		#content => 'this is my motd file managed by content attribute',
